323593d72467392f55bd8a2fa939850d40896ba0
pengcong <ddxgz19880813@gmail.com>
2015-05-14 16:13:23 +0800

    fixed bug, tested on server

---
+++ surveillance/tests/test_restapi.py
@@ -0,0 +1,16 @@
+# -*- coding: utf8 -*-
+from __future__ import absolute_import, division, print_function
+import six
+
+import falcon.testing as testing
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+class TestEventListener(testing.TestBase):
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
