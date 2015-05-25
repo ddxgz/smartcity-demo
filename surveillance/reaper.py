31d49fdb79252ac0fed2be598e739262368c04d5
pengcong <ddxgz19880813@gmail.com>
2015-05-25 17:22:58 +0800

    add remove upload and concats

--- surveillance/reaper.py
+++ surveillance/reaper.py
@@ -14,6 +14,12 @@ from videoedit import get_file_with_prefix
 logging.basicConfig(level=logging.DEBUG)
 
 
+def remove_all(path2file):
+    files =  os.listdir(path2file)
+    for afile in files:
+        os.remove(path2file+afile)
+        logging.debug('removed: %s' % path2file+afile)
+
 @funclogger('------------editting----------')
 def videos_reaper(num_threshold):
     conf = Config()
@@ -28,6 +34,14 @@ def videos_reaper(num_threshold):
         for old_video in old_videos:
             os.remove(conf.video_dir+old_video)
             logging.debug('removed: %s' % conf.video_dir+old_video)
+    # uploaded_files =  os.listdir(conf.video_dir+'upload/')
+    # for old_video in uploaded_files:
+    #     os.remove(conf.video_dir+'upload/'+old_video)
+    #     logging.debug('removed upload: %s' 
+    #         % conf.video_dir+'upload/'+old_video)
+    remove_all(conf.video_dir+'upload/')
+    remove_all(conf.video_dir+'concats/')
+
 
 def container_reaper(num_threshold):
     conf = Config()
@@ -40,6 +54,7 @@ def container_reaper(num_threshold):
     if int(container_head['x-container-object-count']) > num_threshold:
         logging.debug('objects num: %s, delete container...' % 
             container_head['x-container-object-count'])
+        time.sleep(5)
         conn.delete_container(conf.container_video)
         time.sleep(5)
         conn.put_container(conf.container_video)
@@ -58,9 +73,9 @@ if __name__ == '__main__':
     # conn.put_object(test_con, 'file1', file1)
     # conn.head_account()
 
-    # container_reaper(80)
-    while 1:
-        time.sleep(60*60)
-        logging.debug('reaping videos....')
-        videos_reaper(100)
-        container_reaper(40)
+    container_reaper(10)
+    # while 1:
+    #     time.sleep(60*60)
+    #     logging.debug('reaping videos....')
+    #     videos_reaper(100)
+    #     container_reaper(40)
